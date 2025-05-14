c0b4043722c9d1478241b1a16c7883e0570ed7ba
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-05-14 03:07:02 +0300

    initial commit

--- main.js
+++ main.js
@@ -1,54 +1,211 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const logOutBtn = document.getElementById("logout-link");
+  const logInBtn = document.getElementById("login-link");
   const imageUrl = document.getElementById("imageUrl");
   const postText = document.getElementById("postText");
-  const submitPost = document.getElementById("submit-post");
+  const postForm = document.getElementById("post-form");
 
-  const getPosts = async () => {
-    const res = await fetch("https://68219a10259dad2655afc1c9.mockapi.io/post");
-    const data = await res.json();
-    dsipalyPosts(data);
+  // Display the right btn in navbar
+  if (localStorage.getItem("username")) {
+    document.getElementById("logout-link").className =
+      "d-flex btn btn-outline-danger";
+  } else {
+    document.getElementById("login-link").className = "d-flex btn btn-success";
+  }
+
+  // Log user out
+  logOutBtn.onclick = () => {
+    localStorage.removeItem("username");
+    window.location.reload();
   };
 
-  //  Send a POST request to the API
-  submitPost.addEventListener("click", async () => {
+  const delPosts = async (postId) => {
+    if ((window, confirm("Are you sure you want to delete this post?"))) {
+      const res = await fetch(
+        `https://68219a10259dad2655afc1c9.mockapi.io/post/${postId}`,
+        {
+          method: "DELETE",
+        }
+      );
+      getPosts();
+    }
+  };
+
+  const delCmnt = async (post, index) => {
+    const confirm = window.confirm("Are you sure you want to delete this post?");
+    if (!confirm) return;
+
+    const updatedCmnts = post.comment.filter((_, i) => i !== index)
+
     const res = await fetch(
-      "https://68219a10259dad2655afc1c9.mockapi.io/post",
+      `https://68219a10259dad2655afc1c9.mockapi.io/post/${post.id}`,
       {
-        method: "POST",
+        method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          imageUrl: imageUrl.value,
-          text: postText.value,
-          comment: [],
+          ...post,
+          comment: updatedCmnts
         }),
       }
     );
     getPosts();
-  });
+  };
 
-  function dsipalyPosts(posts) {
+  const renderPosts = (posts) => {
     const container = document.getElementById("posts-container");
+
+    // Reset
     container.innerHTML = "";
-    const commentsDiv = document.createElement("div");
-    const comeentUl = document.createElement("ul");
-    posts.forEach((item) => {
+
+    posts.reverse().forEach((post) => {
+      const outerDiv = document.createElement("div");
       const card = document.createElement("div");
-      card.className = "card";
+      const username = document.createElement("h4");
       const img = document.createElement("img");
-      img.src = item.imageUrl;
-      img.style.width = "150px";
       const title = document.createElement("h4");
-      title.innerText = item.text;
-      const commentLi = document.createElement("li");
+      const postBody = document.createElement("div");
+      const postText = document.createElement("p");
+      const deleteBtn = document.createElement("button");
+      const commentsDiv = document.createElement("div");
+      const comInput = document.createElement("input");
+      const sendCom = document.createElement("button");
+      const comList = document.createElement("ul");
+      const addComDiv = document.createElement("div");
+
+      outerDiv.classList.add("col-12", "col-md-6", "mb-4");
+      commentsDiv.classList.add("col-md-4", "com-div");
+      addComDiv.classList.add("add-com-div");
+      addComDiv.appendChild(comInput);
+      addComDiv.appendChild(sendCom);
+      comList.classList.add("mt-5", "list-unstyled")
+      comInput.classList.add("com-input");
+      comInput.placeholder = "Add comment...."
+      sendCom.classList.add("btn", "btn-secondary", "send-com");
+      card.classList.add("card", "p-3");
+      username.classList.add("card-title", "post-username");
+      img.classList.add("card-img-top", "my-3");
+      title.classList.add("card-title");
+      postBody.classList.add("card-body");
+      postText.classList.add("card-text");
+      commentsDiv.classList.add("mb-4");
+      deleteBtn.classList.add("btn", "btn-outline-danger")
+      username.innerText = post.username;
+      img.src = post.imageUrl;
+      title.innerText = post.text;
+
+      sendCom.innerText = "Send";
+      deleteBtn.innerText = "Delete"
+      const renderCmnts = () => {
+        comList.innerText = "";
+        post.comment.map((cmnt, index) => {
+          const cmntLi = document.createElement("li");
+          const cmntUser = document.createElement("h3");
+          const cmntContent = document.createElement("h4");
+          const delCmntBtn = document.createElement("button");
+          const innerComDiv = document.createElement("div");
+          innerComDiv.classList.add("inner-com-div", "d-flex", "justify-content-between")
+          innerComDiv.appendChild(cmntContent);
+
+          delCmntBtn.classList.add("btn", "btn-outline-danger");
+          delCmntBtn.innerText = "Delete";
+          cmntUser.innerText = cmnt.username;
+          cmntContent.innerText = cmnt.comText;
+
+          cmntLi.classList.add("border", "p-3", "m-2", "bg-white")
+
+          cmntLi.appendChild(cmntUser);
+          cmntLi.appendChild(innerComDiv);
+          comList.appendChild(cmntLi);
+          
+          if (localStorage.getItem("username") === cmnt.username) {
+          innerComDiv.appendChild(delCmntBtn);
+            delCmntBtn.onclick = () => delCmnt(post, index)
+          }
+          
+        });
+      };
+      renderCmnts();
+      postBody.appendChild(postText);
+      postBody.appendChild(deleteBtn);
+      card.appendChild(username);
       card.appendChild(img);
       card.appendChild(title);
-      comeentUl.appendChild(commentLi);
-      card.appendChild(comeentUl);
-      container.appendChild(card);
-    });
-  }
+      card.appendChild(postBody);
+      outerDiv.appendChild(card);
+      commentsDiv.appendChild(addComDiv);
 
+      commentsDiv.appendChild(comList);
+      container.appendChild(outerDiv);
+      container.appendChild(commentsDiv);
+
+      // Handle add comment
+      sendCom.onclick = async () => {
+        const res = await fetch(
+          `https://68219a10259dad2655afc1c9.mockapi.io/post/${post.id}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              ...post,
+              comment: [
+                ...post.comment,
+                {
+                  username: localStorage.getItem("username"),
+                  comText: comInput.value,
+                },
+              ],
+            }),
+          }
+        );
+
+        const updatedPost = await res.json();
+        post.comment = updatedPost.comment;
+        renderCmnts();
+      };
+
+      // Handle post delete
+      deleteBtn.onclick = () => delPosts(post.id);
+      if (post.username === localStorage.getItem("username")) {
+        deleteBtn.style.display = "inline-block";
+      } else {
+        deleteBtn.style.display = "none";
+      }
+    });
+  };
+
+  const getPosts = async () => {
+    const res = await fetch("https://68219a10259dad2655afc1c9.mockapi.io/post");
+    const posts = await res.json();
+    renderPosts(posts);
+  };
+
+  //  Create a new post
+  postForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        "https://68219a10259dad2655afc1c9.mockapi.io/post",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: localStorage.getItem("username"),
+            imageUrl: imageUrl.value,
+            text: postText.value,
+            comment: [],
+          }),
+        }
+      );
+      getPosts();
+    } catch (err) {
+      console.error(err);
+    }
+  });
   getPosts();
 });
